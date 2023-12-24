6234a9f75b0fabb622b2d6895ec6708cad87bd3b
Arbaz Shaikh <arbazshaikh5138@gmail.com>
2023-12-24 19:50:14 +0530

    project setup

---
+++ src/test/java/com/project/A1Furnitures/A1FurnituresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.A1Furnitures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A1FurnituresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
